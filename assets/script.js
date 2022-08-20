e65a4d330da851e3fcbde205d59a7f24ff6767ba
AKam <andy.kaminski42@gmail.com>
2022-08-20 17:45:02 -0400

    Added fetch command and api url

--- assets/script.js
+++ assets/script.js
@@ -1,1 +1,9 @@
+// API Key
 var apiKey = "347512a67c1356d53c95e3fcd968bdac";
+
+// Input Variables
+var city;
+
+// Fetch
+var requestURL = 'https://api.openweathermap.org/data/2.5/weather?q=' + city + '&appid=' + apiKey;
+fetch(requestURL);
