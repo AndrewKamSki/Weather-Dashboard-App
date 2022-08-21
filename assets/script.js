b25423f5a22b634d5a0d4533390287526824f39f
AKam <andy.kaminski42@gmail.com>
2022-08-21 18:24:47 -0400

    Created .then statements to display to console, created index.html and style.css files

--- assets/script.js
+++ assets/script.js
@@ -2,8 +2,14 @@
 var apiKey = "347512a67c1356d53c95e3fcd968bdac";
 
 // Input Variables
-var city;
+var city="Atlanta";
 
 // Fetch
 var requestURL = 'https://api.openweathermap.org/data/2.5/weather?q=' + city + '&appid=' + apiKey;
-fetch(requestURL);
+fetch(requestURL)
+  .then(function(response) {
+    return response.json();
+  })
+  .then(function(info) {
+    console.log(info);
+  })
