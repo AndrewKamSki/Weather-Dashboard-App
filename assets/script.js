7e2b93e7202f706df96128384209b640af92e763
AKam <andy.kaminski42@gmail.com>
2022-08-24 09:54:18 -0400

    Finished having the code populate for current date, working on 5 day forecast next

--- assets/script.js
+++ assets/script.js
@@ -1,15 +1,56 @@
 // API Key
 var apiKey = "347512a67c1356d53c95e3fcd968bdac";
 
-// Input Variables
-var city="Atlanta";
-
-// Fetch
+// Test fetch params single day
+var city = 'Columbus';
 var requestURL = 'https://api.openweathermap.org/data/2.5/weather?q=' + city + '&appid=' + apiKey;
-fetch(requestURL)
-  .then(function(response) {
-    return response.json();
-  })
-  .then(function(info) {
-    console.log(info);
-  })
+
+// Test fetch for 5 day
+var requestURL2 = 'https://api.openweathermap.org/data/2.5/forecast?q=' + city + '&appid=' + apiKey;
+// Test Fetchs
+// fetch(requestURL)
+//   .then(function(response) {
+//     return response.json();
+//   })
+//   .then(function(info) {
+//     console.log(info);
+//   })
+
+// fetch(requestURL2)
+//   .then(function(response) {
+//     return response.json();
+//   })
+//   .then(function(info) {
+//     console.log(info);
+//   })
+
+function getWeather(event) {
+  event.preventDefault();
+  var city = $('#userInput').val();
+  var requestURL = 'https://api.openweathermap.org/data/2.5/weather?q=' + city + '&exclude=minutely,hourly&units=imperial&appid=' + apiKey;
+  fetch(requestURL)
+    .then(function(response) {
+      return response.json();
+    })
+    .then(function(info) {
+      console.log(info);
+      var temp = info.main.temp;
+      var wind = info.wind.speed;
+      var humidity = info.main.humidity;
+      var dateUX = info.dt;
+      var date = moment(dateUX, 'X').format('M/D/YYYY')
+      var icon = info.weather[0].icon;
+      var iconImg = document.createElement("img");
+      iconImg.src = 'https://openweathermap.org/img/wn/' + icon + '@2x.png';
+      $('#cityName').text(city + ' (' + date + ')');
+      $('#cityName').append(iconImg)
+      $('#temp').text('Temp: ' + temp + '°F');
+      $('#wind').text('Wind: ' + wind + ' MPH');
+      $('#humidity').text('Humidity: ' + humidity +' %');
+
+      // var uvIndex = info.current.uvi;
+      // console.log(uvIndex)
+    })
+}
+
+$('#button-addon2').on('click', getWeather)
